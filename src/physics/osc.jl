4f938c353736ae1a649aba8cea4d0702c0fb5b00
sofia <you@example.com>
2025-05-20 15:29:34 +0200

    20/05

--- src/physics/osc.jl
+++ src/physics/osc.jl
@@ -74,6 +74,12 @@ end
     three_flavour::ThreeFlavour = ThreeFlavour()
     N_KK::Int = 5
 end
+@kwdef struct NND <: FlavourModel       'New'
+    three_flavour::ThreeFlavour = ThreeFlavour()
+    N_KK::Int = 5
+end
+
+
 
 @kwdef struct OscillationConfig{F<:FlavourModel, I<:InteractionModel, P<:PropagationModel, S<:StateSelector}
     flavour::F = ThreeFlavour()
@@ -176,6 +182,27 @@ function get_priors(cfg::ADD)
     priors = OrderedDict{Symbol, Distribution}(pairs(std))
     priors[:m₀] = LogUniform(ftype(1e-3),ftype(1))
     priors[:ADD_radius] = LogUniform(ftype(1e-3),ftype(1))
+    NamedTuple(priors)
+end
+
+
+   
+function get_params(cfg::NND)  'New'
+    std = get_params(cfg.three_flavour)
+    params = OrderedDict(pairs(std))
+    params[:m₀] = ftype(0.01)
+    params[:N] = ftype(1)
+    params[:r] = ftype(1)
+    NamedTuple(params)
+end
+
+function get_priors(cfg::NND)    'New'
+    std = get_priors(cfg.three_flavour)
+    priors = OrderedDict(pairs(std))
+    priors = OrderedDict{Symbol, Distribution}(pairs(std))
+    priors[:m₀] = LogUniform(ftype(1e-3),ftype(1))
+    priors[:N] = LogUniform(ftype(1),ftype(100))
+    priors[:r] = LogUniform(ftype(0),ftype(1))
     NamedTuple(priors)
 end
 
@@ -504,6 +531,62 @@ function get_matrices(cfg::ADD)
         h, U = eigen(aaMM)
         h = h / (params.ADD_radius^2 * umev^2.)
         return U, h
+    end
+end
+
+
+
+
+function get_matrices(cfg::NND)
+
+    function get_Nnaturalness(params)
+
+        N = round(Int,(params["NN_N"]))
+        
+        r= params["r"]
+        matrix = zeros(N, N)
+    
+        for i in 1:N
+            for j in 1:N
+                if i == j
+                matrix[i, j] = sqrt(2*(i-1)+r) * sqrt(2*(j-1)+r) + (1/N)*sqrt(2*(i-1)+r) * sqrt(2*(j-1)+r)  
+                else
+                matrix[i, j] = sqrt(2*(i-1)+r) * sqrt(2*(j-1)+r)
+                end
+            
+            end
+        end
+        eigvalues, Usector =eigen(matrix)
+        #println(eigvalues)
+        m1, m2, m3 = get_abs_masses(params)
+        #println(m1)
+        #println(m2)
+        #println(m3)
+        osc = OscillationParameters(3*N);
+        setΔm²!(osc, 2=>1, m2^2-m1^2);
+        setΔm²!(osc, 3=>1, m3^2-m1^2);
+        for i in 2:N
+        m_s1sq = (N*eigvalues[i]) * m1^2
+        m_s2sq = (N*eigvalues[i]) * m2^2
+        m_s3sq = (N*eigvalues[i]) * m3^2
+        setΔm²!(osc, 3*i-2=>1, m_s1sq-m1^2);
+        setΔm²!(osc, 3*i-1=>1, m_s2sq-m1^2);
+        setΔm²!(osc, 3*i=>1, m_s3sq-m1^2);
+        end
+    #println(osc)
+        H = Hamiltonian(osc)
+
+        U, _ = get_SM(params)
+
+    #display(Usector)
+    #display(U)
+        FinalUmatrix = kron(Usector, U)
+
+
+
+        #display(FinalUmatrix)
+        return FinalUmatrix, H
+    
     end
 end
 
