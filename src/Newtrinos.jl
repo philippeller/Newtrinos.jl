ef8c77b364c82ddcc512854538702afd310517f6
Philipp Eller <peller.phys@gmail.com>
2025-05-21 15:31:31 +0200

    adding molewhacker

--- src/Newtrinos.jl
+++ src/Newtrinos.jl
@@ -6,6 +6,7 @@ abstract type Experiment end
 
 export Physics, Experiment
 export NewtrinosResult, plot
+export make_init_samples, whack_a_moles, whack_many_moles
 
 include("physics/osc.jl")
 using .osc
@@ -13,6 +14,7 @@ include("physics/earth_layers.jl")
 include("physics/atm_flux.jl")
 include("physics/xsec.jl")
 include("analysis/analysis_tools.jl")
+include("analysis/molewhacker.jl")
 include("utils/plotting.jl")
 include("utils/autodiff.jl")
 
