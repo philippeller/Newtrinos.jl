7829575cbac329820ae5ad9b6edbcd11399ada23
Melike Erbek <melikeerbek@Melikes-MacBook-Air.local>
2025-05-30 11:59:17 +0200

    Adding Juno module and first ADD analysis

---
+++ src/experiments/juno/juno.jl
@@ -0,0 +1,397 @@
+module juno
+
+using DataFrames
+using CSV
+using Distributions
+using LinearAlgebra
+using Statistics
+using DataStructures
+using BAT
+using CairoMakie
+using Logging
+using Interpolations
+using Base
+using ForwardDiff
+using DensityInterface
+using InverseFunctions
+using Optimization
+using Optim
+using IterTools
+using DataStructures
+using ADTypes
+using AutoDiffOperators
+using ContentHashes
+import ValueShapes
+using FileIO
+using FillArrays
+using MeasureBase
+using FunctionChains
+using Accessors
+import ..Newtrinos
+
+@kwdef struct JUNO <: Newtrinos.Experiment
+    physics::NamedTuple
+    params::NamedTuple
+    priors::NamedTuple
+    assets::NamedTuple
+    forward_model::Function
+    plot::Function
+end
+
+function configure(physics)
+    physics = (;physics.osc)
+    assets = get_assets(physics)
+    return JUNO(
+        physics = physics,
+        params = get_params(),
+        priors = get_priors(),
+        assets = assets,
+        forward_model = get_forward_model(physics, assets),
+        plot = get_plot(physics, assets)
+    )
+end
+
+function get_params()
+    params = (
+        shape_eps   = 0.0,
+        flux_scale = 1.0,   
+        energy_scale = 1.0,      
+        detection_epsilon = 1.0,
+        geo_rate_norm = 1.0,
+        res_a = 0.0261,
+        res_b = 0.0082 ,
+        res_c = 0.0123,
+        geo_shape_eps = 0.0,
+        )
+end
+
+function get_priors()
+    priors = (
+        shape_eps = Normal(0,1),
+        
+        geo_shape_eps = Normal(0,1),
+        geo_rate_norm = Normal(1.0, 0.30),
+        
+        res_a = Normal(0.0261, 0.0002),
+        
+        detection_epsilon = Normal(1.0, 0.01),
+        
+        flux_scale = Normal(1.0, 0.02), 
+        energy_scale = Normal(1.0, 0.005), 
+        
+        res_b = Normal(0.0082, 0.0001),
+        res_c = Normal(0.0123, 0.0004),            
+          )
+end
+
+
+
+function get_assets(physics, datadir = @__DIR__)
+    @info "Loading juno data"
+
+    AVG_MATTER_DENSITY_G_CM3 = 2.45    
+    ELECTRON_FRACTION_YE = 0.5        
+    NEUTRON_FRACTION_YN = 1.0 - ELECTRON_FRACTION_YE
+    AVG_MOLAR_MASS_G_PER_MOL = 1.0
+    ELECTRON_DENSITY = AVG_MATTER_DENSITY_G_CM3 * ELECTRON_FRACTION_YE / AVG_MOLAR_MASS_G_PER_MOL
+    NEUTRON_DENSITY  = AVG_MATTER_DENSITY_G_CM3 * NEUTRON_FRACTION_YN / AVG_MOLAR_MASS_G_PER_MOL
+
+    L_JUNO = 52.5   
+    L_JUNO_m = L_JUNO * 1e3 
+    
+    resolution_a = 0.0261
+    resolution_b = 0.0082 
+    resolution_c = 0.0123 
+
+    nominal_livetime = 6.0
+    analysis_livetime = 6.0
+
+    GEO_RATE_UNC = 0.30   
+    GEO_SHAPE_UNC_FRACTION = 0.05 
+
+    Delta_E = 20e-3   # 20 keV bins
+    
+    df_observed = CSV.read(joinpath(datadir,"sm_asimov.csv"), DataFrame; header=true, types=Dict("N" => Float64, "Ec" => Float64))
+    observed = convert(Vector{Float64}, df_observed.N)    
+    df_no_osc = CSV.read(joinpath(datadir,"spectrum_noosc.csv"), DataFrame, header=["E [MeV]", "Events"])
+    df_shape = CSV.read(joinpath(datadir,"shape_uncertainty_TAO.csv"), DataFrame)
+    rename!(df_shape, [:energy_MeV, :rel_unc])
+    df_response = CSV.read(joinpath(datadir,"detector_nonlinear_response.csv"), DataFrame, header=["E_deposited_MeV", "response"])
+    df_backgrounds = CSV.read(joinpath(datadir,"geoneutrino_background.csv"), DataFrame, header=["E_visible_MeV", "events_per_day_per_20keV"])
+    
+    df_shape.rel_unc .= df_shape.rel_unc ./ 100.0
+    
+    df_shape2 = combine(DataFrames.groupby(df_shape, :energy_MeV),
+                        :rel_unc => mean => :Δshape)
+    sort!(df_shape2, :energy_MeV)
+    
+    E_shape = df_shape2.energy_MeV
+    Δshape = df_shape2.Δshape
+    
+    shape_unc_interp = LinearInterpolation(
+         E_shape, Δshape;
+         extrapolation_bc = 0.0
+   )
+
+    df_no_osc.Events = df_no_osc.Events .* (analysis_livetime / nominal_livetime) .* (20.0 / 1000.0)  # adjst no_osc events for livetime and bin width (20 keV)
+        
+    df_no_osc_sorted = combine(DataFrames.groupby(df_no_osc, "E [MeV]"), :Events => sum => :Events)
+    sort!(df_no_osc_sorted, "E [MeV]") 
+    
+    E_arr_neutrino_raw = df_no_osc_sorted."E [MeV]"
+    no_osc_arr_raw = df_no_osc_sorted.Events
+    
+    no_osc_interp = LinearInterpolation(E_arr_neutrino_raw, no_osc_arr_raw, extrapolation_bc=0)
+        
+    df_response_sorted = combine(DataFrames.groupby(df_response, "E_deposited_MeV"), :response => mean => :response)
+    sort!(df_response_sorted, "E_deposited_MeV") 
+    E_deposited_MeV_raw = df_response_sorted.E_deposited_MeV
+    response_arr_raw = df_response_sorted.response
+    
+    IBD_KINEMATIC_OFFSET = 0.782  # E_vis = E_nu - (m_n - m_p - m_e) ≈ 0.782 MeV
+
+    E_arr_deposited_for_response = E_arr_neutrino_raw .- 0.782
+    
+    nonlinear_response_interp = LinearInterpolation(E_deposited_MeV_raw, response_arr_raw, extrapolation_bc=Interpolations.Flat())
+    E_arr_visible_from_raw = nonlinear_response_interp.(E_arr_deposited_for_response) .* E_arr_deposited_for_response
+    
+    sort_indices = sortperm(E_arr_visible_from_raw)
+    E_arr_visible_from_raw_sorted = E_arr_visible_from_raw[sort_indices]
+    E_arr_neutrino_for_inverse_sorted = E_arr_neutrino_raw[sort_indices]
+    
+    unique_indices = unique(i -> E_arr_visible_from_raw_sorted[i], 1:length(E_arr_visible_from_raw_sorted))
+    E_arr_visible_final_unique = E_arr_visible_from_raw_sorted[unique_indices]
+    E_arr_neutrino_for_inverse_final_unique = E_arr_neutrino_for_inverse_sorted[unique_indices]
+    
+    
+    visible_to_neutrino_interp = LinearInterpolation(E_arr_visible_final_unique, E_arr_neutrino_for_inverse_final_unique, extrapolation_bc=Interpolations.Flat()) 
+    
+    df_backgrounds_sorted = combine(DataFrames.groupby(df_backgrounds, "E_visible_MeV"), :events_per_day_per_20keV => sum => :events_per_day_per_20keV)
+    sort!(df_backgrounds_sorted, "E_visible_MeV")
+    E_visible_MeV_bkg = df_backgrounds_sorted.E_visible_MeV
+    background_arr_bkg = df_backgrounds_sorted.events_per_day_per_20keV .* analysis_livetime .* 365
+    
+    background_interp = LinearInterpolation(E_visible_MeV_bkg, background_arr_bkg, extrapolation_bc=0)
+    
+    reactors = DataFrame(
+        Reactor = ["Taishan", "Taishan", "Yangjiang", "Yangjiang", "Yangjiang", "Yangjiang", "Yangjiang", "Yangjiang", "Daya Bay"],
+        Core = [1, 2, 1, 2, 3, 4, 5, 6, 0],
+        Power_GW_th = [4.6, 4.6, 2.9, 2.9, 2.9, 2.9, 2.9, 2.9, 17.4],
+        Baseline_m = 1e3 .* [52.77, 52.64, 52.74, 52.82, 52.41, 52.49, 52.11, 52.19, 215.0],
+        IBD_Rate_per_day = [7.5, 7.6, 4.8, 4.7, 4.8, 4.8, 4.9, 4.9, 3.0], 
+        Relative_Flux_percent = [16.0, 16.1, 10.1, 10.1, 10.3, 10.2, 10.4, 10.4, 6.4] 
+    )
+    fluxes_val = reactors.Power_GW_th ./ (reactors.Baseline_m .^ 2)
+    reactors.Flux_Factor = fluxes_val ./ sum(fluxes_val)
+    
+    N_points = round(Int, (E_arr_visible_final_unique[end] - E_arr_visible_final_unique[1]) / Delta_E)
+    
+    if N_points <= 0
+        @warn "N_points for E_bins_visible is $N_points. Setting to 1."
+        N_points = 1 
+    end
+    
+    E_bins_visible = if N_points > 1
+        collect(range(E_arr_visible_final_unique[1], E_arr_visible_final_unique[end], length=N_points))
+    else
+        [(E_arr_visible_final_unique[1] + E_arr_visible_final_unique[end]) / 2.0] 
+    end
+    
+    E_bins_neutrino = visible_to_neutrino_interp.(E_bins_visible)
+    
+    
+    N_smear = 400
+    E_smear_start = isempty(E_arr_visible_final_unique) ? 1.0 : E_arr_visible_final_unique[1] - Delta_E/2
+    E_smear_end = isempty(E_arr_visible_final_unique) ? 8.0 : E_arr_visible_final_unique[end] + Delta_E/2
+    
+    if E_smear_start >= E_smear_end 
+        @warn "E_arr_smear start is not less than end. Adjsting."
+        E_smear_end = E_smear_start + N_smear * 1e-3 
+    end
+    
+    E_arr_smear = collect(range(E_smear_start, E_smear_end, length=N_smear))
+    
+    assets = (;
+        visible_to_neutrino_interp,
+        no_osc_interp,
+        shape_unc_interp,
+        background_interp,
+        reactors,
+        AVG_MATTER_DENSITY_G_CM3,
+        ELECTRON_FRACTION_YE,
+        NEUTRON_FRACTION_YN,
+        AVG_MOLAR_MASS_G_PER_MOL,
+        ELECTRON_DENSITY,
+        NEUTRON_DENSITY,
+        GEO_SHAPE_UNC_FRACTION,
+        E_bins_visible,
+        observed,
+        )    
+    
+end
+
+function smear(E_arr_smear_local, smear_arr_in, sigma_arr; width=10, E_scale=1.0, E_bias=0.0)
+    l = length(smear_arr_in)
+    out = zeros(eltype(smear_arr_in), l)
+
+    for i in 1:l
+        e_center = E_arr_smear_local[i] * E_scale + E_bias 
+        
+        norm_val = 0.0
+        sum_val = 0.0
+        
+        
+        j_min_loop = max(1, i - width)
+        j_max_loop = min(l, i + width)
+
+        for j in j_min_loop:j_max_loop
+
+            coeff = (1 / sigma_arr[j]) * exp(-0.5 * ((e_center - E_arr_smear_local[j]) / sigma_arr[j])^2)
+            norm_val += coeff
+            sum_val += coeff * smear_arr_in[j]
+        end
+        if norm_val > 1e-10 
+            out[i] = sum_val / norm_val
+        else
+            out[i] = 0.0 
+        end
+    end
+    return out
+end
+
+function smear_average(E_arr_smear_local, smear_arr, E_bins, Delta_E_local)
+    E_i_bins = E_bins .- Delta_E_local / 2
+    E_f_bins = E_bins .+ Delta_E_local / 2
+    smear_average_out = zeros(eltype(smear_arr), length(E_bins))
+    for i in 1:length(E_bins)
+        idx = (E_arr_smear_local .> E_i_bins[i]) .& (E_arr_smear_local .<= E_f_bins[i]) # find indices in E_arr_smear_local that fall into the current bin
+        selected_values = smear_arr[idx]
+        smear_average_out[i] = length(selected_values) > 0 ? mean(selected_values) : zero(eltype(smear_arr))
+    end
+    return smear_average_out
+end
+
+function osc_calc(params, physics, assets)
+    
+    E_vis_corr = assets.E_bins_visible .* params.energy_scale
+    E_nu = assets.visible_to_neutrino_interp.(E_vis_corr)
+
+    L_km = assets.reactors.Baseline_m ./ 1e3
+    
+    P_ee = physics.osc.osc_prob(E_nu./1e3, L_km, params; anti=true)[:, :, 1, 1] 
+    
+    prob_weighted_flat = vec(sum(P_ee .* assets.reactors.Flux_Factor', dims=2))
+
+    pro = (;
+            P_ee,
+            prob_weighted_flat
+        )
+end
+
+function get_expected(params, physics, assets)
+    
+    E_vis_corr = assets.E_bins_visible .* params.energy_scale
+    E_nu = assets.visible_to_neutrino_interp.(E_vis_corr)
+
+    L_km = assets.reactors.Baseline_m ./ 1e3
+    
+    P_ee = physics.osc.osc_prob(E_nu./1e3, L_km, params; anti=true)[:, :, 1, 1] 
+    
+    prob_weighted_flat = vec(sum(P_ee .* assets.reactors.Flux_Factor', dims=2))
+
+    unosc_counts_at_E_nu = assets.no_osc_interp.(E_nu)
+
+    spectrum_before_reactor_shape_and_smearing = unosc_counts_at_E_nu .* prob_weighted_flat
+
+    Δshape_values_signal = assets.shape_unc_interp.(E_vis_corr)
+    spectrum_with_reactor_shape_sys = spectrum_before_reactor_shape_and_smearing .* (1.0 .+ params.shape_eps .* Δshape_values_signal)
+
+    current_res_a = params.res_a
+    current_res_b = params.res_b
+    current_res_c = params.res_c
+    
+    sigma_res_val = @. sqrt(current_res_a^2 * abs(E_vis_corr) + current_res_b^2 * E_vis_corr^2 + current_res_c^2)
+    sigma_res_val = max.(sigma_res_val, 1e-9)
+
+    smeared_signal_spectrum = smear(E_vis_corr, spectrum_with_reactor_shape_sys, sigma_res_val, width=200) # width = 15 befofe
+    
+    signal_counts = smeared_signal_spectrum
+    signal_counts .*= params.flux_scale * params.detection_epsilon
+
+    final_signal_counts = max.(signal_counts, 0.0)
+
+    nominal_geo_counts = assets.background_interp.(E_vis_corr)
+    geo_counts_scaled_rate = nominal_geo_counts .* params.geo_rate_norm
+
+    modified_geo_counts = geo_counts_scaled_rate .* (1.0 .+ params.geo_shape_eps .* assets.GEO_SHAPE_UNC_FRACTION) 
+
+    final_geo_counts = max.(modified_geo_counts, 0.0)
+
+    other_background_counts = zeros(length(E_vis_corr))
+
+    return final_signal_counts .+ final_geo_counts .+ other_background_counts
+
+end
+
+function get_forward_model(physics, assets)
+    function forward_model(params)
+        exp_events = get_expected(params, physics, assets)
+        distprod(Poisson.(exp_events))
+    end
+end
+
+function get_plot(physics, assets)
+
+    function plot_spectra(fitted_params,
+                           true_params_for_data_gen, 
+                           current_experiment_physics,
+                           current_experiment_assets;
+                           title_suffix::String="")
+    
+    model_events_fit = get_expected(fitted_params, current_experiment_physics, current_experiment_assets)
+
+    model_events_true = get_expected(true_params_for_data_gen, current_experiment_physics, current_experiment_assets)
+
+    E_vis_bins = current_experiment_assets.E_bins_visible 
+        
+    fig = Figure(size = (900, 700))
+    ax_spec = Axis(fig[1, 1], 
+                   xlabel=L"E_{vis} \text{ (MeV)}", 
+                   ylabel="Events / Bin",
+                   title="JUNO Spectrum Comparison" * title_suffix)
+
+    CairoMakie.scatter!(ax_spec, E_vis_bins, current_experiment_assets.observed, 
+                        label="Observed Data", color=:black, markersize=6)
+
+    ys_err = sqrt.(max.(current_experiment_assets.observed, 0))
+    ys_err[current_experiment_assets.observed .== 0] .= 1.0
+
+    CairoMakie.errorbars!(ax_spec, E_vis_bins, current_experiment_assets.observed, ys_err, 
+                          color=:black, whiskerwidth=0)
+
+    CairoMakie.lines!(ax_spec, E_vis_bins, model_events_true, 
+                      label="Model (Data Gen. Params)", color=:blue, linestyle=:dash, linewidth=2)
+
+    CairoMakie.lines!(ax_spec, E_vis_bins, model_events_fit, 
+                      label="Model (Fitted Params)", color=:red, linewidth=2)
+
+    axislegend(ax_spec, position=:rt)
+
+    ax_res = Axis(fig[2, 1], 
+                  xlabel=L"E_{vis} \text{ (MeV)}", 
+                  ylabel=L"\frac{\text{Data} - \text{Fit}}{\sigma_{\text{Data}}}",
+                  title="Residuals" * title_suffix)
+    
+    residuals = (current_experiment_assets.observed .- model_events_fit) ./ ys_err
+    residuals[.!isfinite.(residuals)] .= 0 
+    
+    CairoMakie.barplot!(ax_res, E_vis_bins, residuals, color=:gray)
+    CairoMakie.hlines!(ax_res, [0], color=:black, linestyle=:dash)
+    ylims!(ax_res, -5, 5) 
+
+    return fig
+    end
+end
+
+
+end 
