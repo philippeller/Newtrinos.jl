426d01d6a9f30b789db0a71e7a927223a241fd0f
Philipp Eller <peller.phys@gmail.com>
2025-06-03 13:48:21 +0200

    command line args

--- src/analysis/analysis.jl
+++ src/analysis/analysis.jl
@@ -10,67 +10,138 @@ using MeasureBase
 using ADTypes
 using Newtrinos
 using FileIO
-import JLD2
+using Accessors
+using ArgParse
 
-adsel = AutoForwardDiff()
-context = set_batcontext(ad = adsel)
+function parse_command_line()
+    s = ArgParseSettings()
 
-###### CONFIG ######
+    @add_arg_table s begin
+        "--experiments"
+        help = "List of experiments to run"
+        nargs = '+'
+        required = true
 
-# Name for output files etc
-name = "deepcore_Ultranest"
+        "--name"
+        help = "Name for outputs"
+        arg_type = String
+        required = true
 
-# Choice of MCMC, Profile, Scan
-task = "MCMC"
+        "--task"
+        help = "Task to perform: Choice of NestedSampling, ImportanceSampling, Profile, Scan"
+        arg_type = String
+        required = true
 
-# Choose oscillation model
-osc = Newtrinos.osc.standard
+        "--plot"
+        help = "Enable plotting"
+        action = :store_true
+    end
 
-# Choose experiments to include
-modules = [Newtrinos.deepcore]#, Newtrinos.kamland, Newtrinos.dayabay, Newtrinos.minos]
+    return parse_args(s)
+end
 
-# Variables to condition on (=fix)
-conditional_vars = [:θ₁₂, :θ₁₃, :δCP, :Δm²₂₁, :nutau_cc_norm]
+# dynamically construct named tuple from experiment names
+function configure_experiments(experiment_list, physics)
+    pairs = (Symbol(lowercase(exp)) => getproperty(getproperty(Newtrinos, Symbol(lowercase(exp))), :configure)(physics) for exp in experiment_list)
+    return (; pairs...)
+end
 
-# For profile / scan task only: choose scan grid
-vars_to_scan = OrderedDict()
-vars_to_scan[:θ₂₃] = 31
-vars_to_scan[:Δm²₃₁] = 31
+#function main()
 
-###### END CONFIG ######
+    args = parse_command_line()
 
-llhs = [let osc_prob = osc.osc_prob, observed = m.observed 
-        params -> logpdf(m.forward_model(osc_prob)(params), observed)
+    adsel = AutoForwardDiff()
+    context = set_batcontext(ad = adsel)
+
+    name = args["name"]
+
+    osc_cfg = Newtrinos.osc.OscillationConfig(
+        flavour=Newtrinos.osc.ThreeFlavour(ordering=:NO),
+        propagation=Newtrinos.osc.Basic(),
+        states=Newtrinos.osc.All(),
+        interaction=Newtrinos.osc.SI()
+        )
+    osc = Newtrinos.osc.configure(osc_cfg)
+    atm_flux = Newtrinos.atm_flux.configure()
+    earth_layers = Newtrinos.earth_layers.configure()
+    xsec = Newtrinos.xsec.configure()
+
+    physics = (; osc, atm_flux, earth_layers, xsec);
+
+    # Choose experiments to include
+    #experiments = (
+    #    deepcore = Newtrinos.deepcore.configure(physics),
+    #    dayabay = Newtrinos.dayabay.configure(physics),
+    #    kamland = Newtrinos.kamland.configure(physics),
+    #    minos = Newtrinos.minos.configure(physics),
+    #    orca = Newtrinos.orca.configure(physics),
+    #);
+    
+    experiments = configure_experiments(args["experiments"], physics)
+
+    # Variables to condition on (=fix)
+    #conditional_vars = [:θ₁₂, :θ₁₃, :δCP, :Δm²₂₁, :nutau_cc_norm]
+    conditional_vars = []
+
+    # For profile / scan task only: choose scan grid
+    vars_to_scan = OrderedDict()
+    vars_to_scan[:θ₂₃] = 31
+    vars_to_scan[:Δm²₃₁] = 31
+
+    ###### END CONFIG ######
+
+    likelihood = Newtrinos.generate_likelihood(experiments);
+
+    p = Newtrinos.get_params(experiments)
+    priors = Newtrinos.get_priors(experiments)
+
+    function condition(priors::NamedTuple, conditional_vars, p)
+        for var in conditional_vars
+            @reset priors[var] = p[var]
         end
-        for m in modules]
+        priors
+    end
 
-llh = logfuncdensity(params -> sum([f(params) for f in llhs]))
+    priors = condition(priors, conditional_vars, p)
+        
+    if lowercase(args["task"]) == "nestedsampling"
+        #import NestedSamplers
+        import UltraNest
+        prior = distprod(;priors...)
+        posterior = PosteriorMeasure(likelihood, prior)
+        samples = bat_sample(posterior, ReactiveNestedSampling()).result
+        #samples = bat_sample(posterior, EllipsoidalNestedSampling()).result
+        #samples = bat_sample(posterior, MCMCSampling(mcalg = MetropolisHastings(), nsteps = 10^4, nchains = 4)).result
+        FileIO.save(name * ".jld2", Dict("samples" => samples))
+        
+    elseif lowercase(args["task"]) == "importancesampling"
+        prior = distprod(;priors...)
+        posterior = PosteriorMeasure(likelihood, prior)
+        init_samples =  make_init_samples(posterior, 10, 100_000)
+        FileIO.save(name * "_init_samples.jld2", Dict(String(a)=>init_samples[a] for a in keys(init_samples)))
+        whack_samples = whack_many_moles(posterior, init_samples, target_efficiency=0.09)
+        FileIO.save(name * ".jld2", Dict(String(a)=>whack_samples[a] for a in keys(whack_samples)))
+    else
+        if lowercase(args["task"]) == "profile"
+            result = Newtrinos.profile(likelihood, priors, vars_to_scan, p, cache_dir=name)
 
-params_dict = merge(osc.params, [m.params for m in modules]...)
-priors_dict = merge(osc.priors, [m.priors for m in modules]...)
+        elseif lowercase(args["task"]) == "scan"
+            result = Newtrinos.scan(likelihood, priors, vars_to_scan, p)
+        end 
+        FileIO.save(name * ".jld2", Dict("result" => result))
 
-for var in conditional_vars
-    priors_dict[var] = params_dict[var]
-end
+        if args["plot"]
+            using CairoMakie
+            fig = Figure()
+            ax = Axis(fig[1,1])
+            plot!(ax, result)
+            ax.xlabel = String(collect(keys(vars_to_scan))[1])
+            ax.ylabel = String(collect(keys(vars_to_scan))[2])
+            ax.title = join(args["experiments"], " + ")
+            save(name * ".png", fig)
+        end
+    end
 
-if task == "MCMC"
-    #import NestedSamplers
-    import UltraNest
-    prior = distprod(;priors_dict...)
-    posterior = PosteriorMeasure(llh, prior)
-    samples = bat_sample(posterior, ReactiveNestedSampling()).result
-    #samples = bat_sample(posterior, EllipsoidalNestedSampling()).result
-    #samples = bat_sample(posterior, MCMCSampling(mcalg = MetropolisHastings(), nsteps = 10^4, nchains = 4)).result
-    FileIO.save(name * ".jld2", Dict("samples" => samples))
+#end
 
-else
-    if task == "Profile"
-        result = Newtrinos.profile(llh, priors_dict, vars_to_scan, params_dict, cache_dir=name)
-
-    elseif task == "Scan"
-        result = Newtrinos.scan(llh, priors_dict, vars_to_scan, params_dict)
-
-    end 
-    FileIO.save(name * ".jld2", Dict("result" => result))
-
-end
+#main()
