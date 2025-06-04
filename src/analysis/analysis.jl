4487992958173772b137a7e8022ee2c93123257e
Philipp Eller <peller.phys@gmail.com>
2025-06-04 13:00:01 +0200

    plotting functionality for cmd line analysis

--- src/analysis/analysis.jl
+++ src/analysis/analysis.jl
@@ -22,6 +22,11 @@ function parse_command_line()
         nargs = '+'
         required = true
 
+        "--ordering"
+        help = "NMO: either NO or IO"
+        arg_type = String
+        default = "NO"      
+
         "--name"
         help = "Name for outputs"
         arg_type = String
@@ -46,102 +51,102 @@ function configure_experiments(experiment_list, physics)
     return (; pairs...)
 end
 
-#function main()
 
-    args = parse_command_line()
+args = parse_command_line()
 
-    adsel = AutoForwardDiff()
-    context = set_batcontext(ad = adsel)
+adsel = AutoForwardDiff()
+context = set_batcontext(ad = adsel)
 
-    name = args["name"]
+name = args["name"]
 
-    osc_cfg = Newtrinos.osc.OscillationConfig(
-        flavour=Newtrinos.osc.ThreeFlavour(ordering=:NO),
-        propagation=Newtrinos.osc.Basic(),
-        states=Newtrinos.osc.All(),
-        interaction=Newtrinos.osc.SI()
-        )
-    osc = Newtrinos.osc.configure(osc_cfg)
-    atm_flux = Newtrinos.atm_flux.configure()
-    earth_layers = Newtrinos.earth_layers.configure()
-    xsec = Newtrinos.xsec.configure()
+osc_cfg = Newtrinos.osc.OscillationConfig(
+    flavour=Newtrinos.osc.ThreeFlavour(ordering=Symbol(args["ordering"])),
+    propagation=Newtrinos.osc.Basic(),
+    states=Newtrinos.osc.All(),
+    interaction=Newtrinos.osc.SI()
+    )
+osc = Newtrinos.osc.configure(osc_cfg)
+atm_flux = Newtrinos.atm_flux.configure()
+earth_layers = Newtrinos.earth_layers.configure()
+xsec = Newtrinos.xsec.configure()
 
-    physics = (; osc, atm_flux, earth_layers, xsec);
+physics = (; osc, atm_flux, earth_layers, xsec);
 
-    # Choose experiments to include
-    #experiments = (
-    #    deepcore = Newtrinos.deepcore.configure(physics),
-    #    dayabay = Newtrinos.dayabay.configure(physics),
-    #    kamland = Newtrinos.kamland.configure(physics),
-    #    minos = Newtrinos.minos.configure(physics),
-    #    orca = Newtrinos.orca.configure(physics),
-    #);
+# Choose experiments to include
+#experiments = (
+#    deepcore = Newtrinos.deepcore.configure(physics),
+#    dayabay = Newtrinos.dayabay.configure(physics),
+#    kamland = Newtrinos.kamland.configure(physics),
+#    minos = Newtrinos.minos.configure(physics),
+#    orca = Newtrinos.orca.configure(physics),
+#);
+
+experiments = configure_experiments(args["experiments"], physics)
+
+# Variables to condition on (=fix)
+#conditional_vars = [:θ₁₂, :θ₁₃, :δCP, :Δm²₂₁, :nutau_cc_norm]
+conditional_vars = []
+
+# For profile / scan task only: choose scan grid
+vars_to_scan = OrderedDict()
+#vars_to_scan[:θ₂₃] = 31
+vars_to_scan[:Δm²₃₁] = 31
+
+###### END CONFIG ######
+
+likelihood = Newtrinos.generate_likelihood(experiments);
+
+p = Newtrinos.get_params(experiments)
+priors = Newtrinos.get_priors(experiments)
+
+function condition(priors::NamedTuple, conditional_vars, p)
+    for var in conditional_vars
+        @reset priors[var] = p[var]
+    end
+    priors
+end
+
+priors = condition(priors, conditional_vars, p)
     
-    experiments = configure_experiments(args["experiments"], physics)
+if lowercase(args["task"]) == "nestedsampling"
+    #import NestedSamplers
+    import UltraNest
+    prior = distprod(;priors...)
+    posterior = PosteriorMeasure(likelihood, prior)
+    samples = bat_sample(posterior, ReactiveNestedSampling()).result
+    #samples = bat_sample(posterior, EllipsoidalNestedSampling()).result
+    #samples = bat_sample(posterior, MCMCSampling(mcalg = MetropolisHastings(), nsteps = 10^4, nchains = 4)).result
+    FileIO.save(name * ".jld2", Dict("samples" => samples))
+    
+elseif lowercase(args["task"]) == "importancesampling"
+    prior = distprod(;priors...)
+    posterior = PosteriorMeasure(likelihood, prior)
+    init_samples =  make_init_samples(posterior, 10, 100_000)
+    FileIO.save(name * "_init_samples.jld2", Dict(String(a)=>init_samples[a] for a in keys(init_samples)))
+    whack_samples = whack_many_moles(posterior, init_samples, target_efficiency=0.09)
+    FileIO.save(name * ".jld2", Dict(String(a)=>whack_samples[a] for a in keys(whack_samples)))
+else
+    if lowercase(args["task"]) == "profile"
+        result = Newtrinos.profile(likelihood, priors, vars_to_scan, p, cache_dir=name)
 
-    # Variables to condition on (=fix)
-    #conditional_vars = [:θ₁₂, :θ₁₃, :δCP, :Δm²₂₁, :nutau_cc_norm]
-    conditional_vars = []
+    elseif lowercase(args["task"]) == "scan"
+        result = Newtrinos.scan(likelihood, priors, vars_to_scan, p)
+    end 
+    FileIO.save(name * ".jld2", Dict("result" => result))
 
-    # For profile / scan task only: choose scan grid
-    vars_to_scan = OrderedDict()
-    vars_to_scan[:θ₂₃] = 31
-    vars_to_scan[:Δm²₃₁] = 31
+    if args["plot"]
+        using CairoMakie
+        fig = Figure()
+        ax = Axis(fig[1,1])
+        plot!(ax, result)
+        ax.xlabel = String(collect(keys(vars_to_scan))[1])
 
-    ###### END CONFIG ######
-
-    likelihood = Newtrinos.generate_likelihood(experiments);
-
-    p = Newtrinos.get_params(experiments)
-    priors = Newtrinos.get_priors(experiments)
-
-    function condition(priors::NamedTuple, conditional_vars, p)
-        for var in conditional_vars
-            @reset priors[var] = p[var]
-        end
-        priors
-    end
-
-    priors = condition(priors, conditional_vars, p)
-        
-    if lowercase(args["task"]) == "nestedsampling"
-        #import NestedSamplers
-        import UltraNest
-        prior = distprod(;priors...)
-        posterior = PosteriorMeasure(likelihood, prior)
-        samples = bat_sample(posterior, ReactiveNestedSampling()).result
-        #samples = bat_sample(posterior, EllipsoidalNestedSampling()).result
-        #samples = bat_sample(posterior, MCMCSampling(mcalg = MetropolisHastings(), nsteps = 10^4, nchains = 4)).result
-        FileIO.save(name * ".jld2", Dict("samples" => samples))
-        
-    elseif lowercase(args["task"]) == "importancesampling"
-        prior = distprod(;priors...)
-        posterior = PosteriorMeasure(likelihood, prior)
-        init_samples =  make_init_samples(posterior, 10, 100_000)
-        FileIO.save(name * "_init_samples.jld2", Dict(String(a)=>init_samples[a] for a in keys(init_samples)))
-        whack_samples = whack_many_moles(posterior, init_samples, target_efficiency=0.09)
-        FileIO.save(name * ".jld2", Dict(String(a)=>whack_samples[a] for a in keys(whack_samples)))
-    else
-        if lowercase(args["task"]) == "profile"
-            result = Newtrinos.profile(likelihood, priors, vars_to_scan, p, cache_dir=name)
-
-        elseif lowercase(args["task"]) == "scan"
-            result = Newtrinos.scan(likelihood, priors, vars_to_scan, p)
-        end 
-        FileIO.save(name * ".jld2", Dict("result" => result))
-
-        if args["plot"]
-            using CairoMakie
-            fig = Figure()
-            ax = Axis(fig[1,1])
-            plot!(ax, result)
-            ax.xlabel = String(collect(keys(vars_to_scan))[1])
+        if length(vars_to_scan) == 1
+            ax.ylabel = "-2ΔLLH"
+        else
             ax.ylabel = String(collect(keys(vars_to_scan))[2])
-            ax.title = join(args["experiments"], " + ")
-            save(name * ".png", fig)
         end
+        ax.title = args["task"] * ": " * join(args["experiments"], " + ")
+        save(name * ".png", fig)
     end
-
-#end
-
-#main()
+end
--- src/utils/plotting.jl
+++ src/utils/plotting.jl
@@ -17,24 +17,38 @@ import Newtrinos.NewtrinosResult
 function plot!(ax, result::NewtrinosResult; max_llh=maximum(result.values.log_posterior), levels=1 .- 2*ccdf(Normal(), 1:3), label=nothing, color=:blue, linestyle=:solid, cmap=:Blues, filled=false, edge=true, transform_x=identity, transform_y=identity)
     neg2dllh = 2*(max_llh .- result.values.log_posterior)
 
-    x = transform_x(result.axes[1])
-    y = transform_y(result.axes[2])
-    
-    if filled
-    contourf!(ax, x, y,
-            neg2dllh,
-            levels=quantile(Chisq(2), levels),
-            colormap=cmap)
-    end
-    if edge
-        contour!(ax, x, y, 
-            neg2dllh, 
-            levels=quantile(Chisq(2), levels),
-            linewidth=2,
+    if length(result.axes) == 1
+        x = transform_x(result.axes[1])
+
+        hlines!(ax, quantile(Chisq(1), levels), color=:black, linestyle=:dot)
+        lines!(ax, x, neg2dllh, linewidth=2,
             color=color,
-            linestyle=linestyle)
-        lines!(ax, [NaN], [NaN], color = color, linestyle=linestyle, label = label)
+            linestyle=linestyle,
+            label=label)
+        
+    elseif length(result.axes) == 2
+        x = transform_x(result.axes[1])
+        y = transform_y(result.axes[2])
+        
+        if filled
+        contourf!(ax, x, y,
+                neg2dllh,
+                levels=quantile(Chisq(2), levels),
+                colormap=cmap)
+        end
+        if edge
+            contour!(ax, x, y, 
+                neg2dllh, 
+                levels=quantile(Chisq(2), levels),
+                linewidth=2,
+                color=color,
+                linestyle=linestyle)
+            lines!(ax, [NaN], [NaN], color = color, linestyle=linestyle, label = label)
+        end
+    else
+        DimensionMismatch("Cannot plot contours in $(length(result.axes)) dimesions")
     end
+        
     ax
 end
 
