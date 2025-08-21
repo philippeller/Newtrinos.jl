fad1f2f8ba0c88389246d531383f2d6620f65f70
sofia <you@example.com>
2025-08-21 18:25:11 +0200

    21/08

--- src/physics/osc.jl
+++ src/physics/osc.jl
@@ -256,7 +256,7 @@ function get_params(cfg::NNM)  #'New'
     std = get_params(cfg.three_flavour)
     params = OrderedDict(pairs(std))
     params[:m₀] = ftype(0.1)
-    params[:N] = ftype(30)
+    params[:N] = ftype(10)
     params[:r] = ftype(1)
     
     NamedTuple(params)
