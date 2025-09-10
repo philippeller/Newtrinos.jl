4e8564b37e26a5aacb352910c0f685c6c829a26d
sofia <you@example.com>
2025-09-10 18:42:26 +0200

    10/09

--- src/physics/osc.jl
+++ src/physics/osc.jl
@@ -244,7 +244,7 @@ function get_priors(cfg::NND)    #'New'
     std = get_priors(cfg.three_flavour)
     priors = OrderedDict{Symbol, Distribution}(pairs(std))
     priors[:m₀] = Uniform(ftype(1e-3),ftype(2)) #LogUniform(ftype(1e-3),ftype(1))
-    priors[:N] = Uniform(ftype(3),ftype(100))
+    priors[:N] = Uniform(ftype(1),ftype(100))
     priors[:r] = Uniform(ftype(0),ftype(1))
 
     NamedTuple(priors)
@@ -256,7 +256,7 @@ function get_params(cfg::NNM)  #'New'
     std = get_params(cfg.three_flavour)
     params = OrderedDict(pairs(std))
     params[:m₀] = ftype(0.1)
-    params[:N] = ftype(40)
+    params[:N] = ftype(30)
     params[:r] = ftype(1)
     
     NamedTuple(params)
@@ -267,7 +267,7 @@ function get_priors(cfg::NNM)    #'New'
     priors = OrderedDict(pairs(std))
     priors = OrderedDict{Symbol, Distribution}(pairs(std))
     priors[:m₀] = Uniform(ftype(1e-7),ftype(0.015)) #Uniform(ftype(1e-7),ftype(2)) 
-    priors[:N] = Uniform(ftype(3),ftype(100))
+    priors[:N] = Uniform(ftype(2),ftype(100))
     priors[:r] = Uniform(ftype(0),ftype(1))
 
     NamedTuple(priors)
